8425eddaf1bd5da140ad618de7fbf6d35633b2d6
PLecicki <p.lecicki@gmail.com>
2022-10-12 19:36:50 +0200

    first commit, add positions to gitignore

---
+++ src/main/java/com/kodilla/sportscentre/SportsCentreApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.sportscentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCentreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsCentreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/sportscentre/SportsCentreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.sportscentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsCentreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
