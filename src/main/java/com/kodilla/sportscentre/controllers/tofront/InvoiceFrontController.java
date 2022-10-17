20212643fc87f6bf5e0c2f11baaf66e3ec314fc4
PLecicki <p.lecicki@gmail.com>
2022-10-17 19:29:23 +0200

    Setting status of invoice endpoint

---
+++ src/main/java/com/kodilla/sportscentre/controllers/tofront/InvoiceFrontController.java
@@ -0,0 +1,27 @@
+package com.kodilla.sportscentre.controllers.tofront;
+
+import com.kodilla.sportscentre.domain.Invoice;
+import com.kodilla.sportscentre.exceptions.InvoiceNotFoundException;
+import com.kodilla.sportscentre.services.InvoiceFrontService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/invoice_status")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class InvoiceFrontController {
+
+    private final InvoiceFrontService invoiceFrontService;
+
+    @PutMapping(value = "/paid/{invoiceId}")
+    public ResponseEntity<Invoice> setPaymentPaidStatus(@PathVariable Long invoiceId) throws InvoiceNotFoundException {
+        return ResponseEntity.ok(invoiceFrontService.setPaymentPaidStatus(invoiceId));
+    }
+
+    @PutMapping(value = "/not_paid/{invoiceId}")
+    public ResponseEntity<Invoice> setPaymentNotPaidStatus(@PathVariable Long invoiceId) throws InvoiceNotFoundException {
+        return ResponseEntity.ok(invoiceFrontService.setPaymentNotPaidStatus(invoiceId));
+    }
+}
---
+++ src/main/java/com/kodilla/sportscentre/services/InvoiceFrontService.java
@@ -0,0 +1,27 @@
+package com.kodilla.sportscentre.services;
+
+import com.kodilla.sportscentre.domain.Invoice;
+import com.kodilla.sportscentre.domain.enums.PaymentStatus;
+import com.kodilla.sportscentre.exceptions.InvoiceNotFoundException;
+import com.kodilla.sportscentre.repositories.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InvoiceFrontService {
+
+    private final InvoiceRepository invoiceRepository;
+
+    public Invoice setPaymentPaidStatus(final Long invoiceId) throws InvoiceNotFoundException {
+        Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow(InvoiceNotFoundException::new);
+        invoice.setPaymentStatus(PaymentStatus.PAID);
+        return invoiceRepository.save(invoice);
+    }
+
+    public Invoice setPaymentNotPaidStatus(final Long invoiceId) throws InvoiceNotFoundException {
+        Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow(InvoiceNotFoundException::new);
+        invoice.setPaymentStatus(PaymentStatus.NOTPAID);
+        return invoiceRepository.save(invoice);
+    }
+}
