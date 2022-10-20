37bc9fd3c74cab3b5d1ffb8d2cf1c7b8dd0a946d
PLecicki <p.lecicki@gmail.com>
2022-10-21 01:51:51 +0200

    Change YouTube API serice test

--- src/test/java/com/kodilla/sportscentre/config/YouTubeConfigTest.java
+++ src/test/java/com/kodilla/sportscentre/config/YouTubeConfigTest.java
@@ -24,63 +24,19 @@ public class YouTubeConfigTest {
     @Value("${youtube.key}")
     private String youTubeKey;
 
-    @Value("${youtube.video.0}")
-    private String videoId0;
-
-    @Value("${youtube.video.1}")
-    private String videoId1;
-
-    @Value("${youtube.video.2}")
-    private String videoId2;
-
-    @Value("${youtube.video.3}")
-    private String videoId3;
-
-    @Value("${youtube.video.4}")
-    private String videoId4;
-
-    @Value("${youtube.video.5}")
-    private String videoId5;
-
-    @Value("${youtube.video.6}")
-    private String videoId6;
-
-    @Value("${youtube.video.7}")
-    private String videoId7;
-
-    @Value("${youtube.video.8}")
-    private String videoId8;
-
-    @Value("${youtube.video.9}")
-    private String videoId9;
+    @Value("${youtube.videos}")
+    private List<String> videos;
 
     @Test
     void youTubeConfigTest() {
         //Given & When
-        List<String> videosList = youTubeConfig.getVideosIdList();
+        List<String> videosList = youTubeConfig.getVideos();
         //Then
         Assertions.assertEquals(youTubeEndpoint, youTubeConfig.getYouTubeEndpoint());
         Assertions.assertEquals(youTubeVideo, youTubeConfig.getYouTubeVideo());
         Assertions.assertEquals(youTubeKey, youTubeConfig.getYouTubeKey());
-        Assertions.assertEquals(videoId0, youTubeConfig.getVideoId0());
-        Assertions.assertEquals(videoId1, youTubeConfig.getVideoId1());
-        Assertions.assertEquals(videoId2, youTubeConfig.getVideoId2());
-        Assertions.assertEquals(videoId3, youTubeConfig.getVideoId3());
-        Assertions.assertEquals(videoId4, youTubeConfig.getVideoId4());
-        Assertions.assertEquals(videoId5, youTubeConfig.getVideoId5());
-        Assertions.assertEquals(videoId6, youTubeConfig.getVideoId6());
-        Assertions.assertEquals(videoId7, youTubeConfig.getVideoId7());
-        Assertions.assertEquals(videoId8, youTubeConfig.getVideoId8());
-        Assertions.assertEquals(videoId9, youTubeConfig.getVideoId9());
-        Assertions.assertEquals(videoId0, videosList.get(0));
-        Assertions.assertEquals(videoId1, videosList.get(1));
-        Assertions.assertEquals(videoId2, videosList.get(2));
-        Assertions.assertEquals(videoId3, videosList.get(3));
-        Assertions.assertEquals(videoId4, videosList.get(4));
-        Assertions.assertEquals(videoId5, videosList.get(5));
-        Assertions.assertEquals(videoId6, videosList.get(6));
-        Assertions.assertEquals(videoId7, videosList.get(7));
-        Assertions.assertEquals(videoId8, videosList.get(8));
-        Assertions.assertEquals(videoId9, videosList.get(9));
+        for (int i=0; i<videosList.size(); i++) {
+            Assertions.assertEquals(videos.get(i), videosList.get(i));
+        }
     }
 }
